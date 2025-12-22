59935007ef515b8f5efb3630d1b0559788e738d6
aldo wiranata <awiranata263@gmail.com>
2025-12-22 11:31:47 +0700

    add login

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { prisma } from "../lib/prisma";
 import { registerValidation, loginValidation } from "../validations";
 import bcrypt from "bcrypt";
+import { email, ZodError } from "zod";
 
 export const register = async (req: Request, res: Response) => {
   try {
@@ -27,7 +28,7 @@ export const register = async (req: Request, res: Response) => {
       data: {
         name: validated.name,
         email: validated.email,
-        password: validated.password,
+        password: hashedPassword,
       },
     });
 
@@ -42,7 +43,76 @@ export const register = async (req: Request, res: Response) => {
       },
     });
   } catch (error) {
-    // ✅ HANDLE ERROR LAIN
+    //zod error
+    if (error instanceof ZodError) {
+      return res.status(400).json({
+        message: "Validation error",
+        errors: error.issues.map((err) => ({
+          field: err.path.join("."),
+          message: err.message,
+        })),
+      });
+    }
+    // server error
+    return res.status(500).json({
+      message: "Internal Server Error",
+      error: error,
+    });
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const validated = loginValidation.parse(req.body);
+
+    const user = await prisma.user.findUnique({
+      where: { email: validated.email },
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        message: "Email or password is incorect",
+      });
+    }
+
+    if (user.status === "NONACTIVE") {
+      return res.status(403).json({
+        message: "Account is not active. Please contact admin",
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      validated.password,
+      user.password
+    );
+
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        message: "Email or password incorect",
+      });
+    }
+
+    return res.status(200).json({
+      message: "Login success",
+      data: {
+        id: user.id,
+        email: user.email,
+        role: user.role,
+        status: user.status,
+      },
+    });
+
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return res.status(400).json({
+        message: "Validation Error",
+        errors: error.issues.map((err) => ({
+          field: err.path.join("."),
+          error: error,
+        })),
+      });
+    }
+
     return res.status(500).json({
       message: "Internal Server Error",
       error: error,
