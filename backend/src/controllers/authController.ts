1cbbb8b4f1b52c18aeab2877dfc0efc823c447d6
aldo wiranata <awiranata263@gmail.com>
2025-12-23 07:26:18 +0700

    add activation user

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -2,14 +2,16 @@ import { Request, Response } from "express";
 import { prisma } from "../lib/prisma";
 import { registerValidation, loginValidation } from "../validations";
 import bcrypt from "bcrypt";
-import { email, ZodError } from "zod";
+import crypto from "crypto";
+import { ZodError } from "zod";
+import { transporter } from "../lib/mailer";
 
 export const register = async (req: Request, res: Response) => {
   try {
-    //1.validate input
+    //validate input
     const validated = registerValidation.parse(req.body);
 
-    //2.check unique email
+    //check unique email
     const existingEmail = await prisma.user.findUnique({
       where: { email: validated.email },
     });
@@ -20,19 +22,38 @@ export const register = async (req: Request, res: Response) => {
       });
     }
 
-    //3.hash password
+    //hash password
     const hashedPassword = await bcrypt.hash(validated.password, 10);
 
-    //4.save user
+    //activation token
+    const activationToken = crypto.randomBytes(32).toString("hex");
+
+    //expire token
+    const activationExpiredAt = new Date(Date.now() + 1000 * 60 * 60);
+
+    //save user
     const user = await prisma.user.create({
       data: {
         name: validated.name,
         email: validated.email,
         password: hashedPassword,
+        activationToken: activationToken,
+        activationExpiredAt: activationExpiredAt,
       },
     });
-
-    //5.response
+    //send activationlink
+    const activationLink = `${process.env.BASE_URL}/auth/activate/${activationToken}`;
+    await transporter.sendMail({
+      to: user.email,
+      subject: "Activate your CashIn account",
+      html: `
+      <h3>Welcome to CashIn</h3>
+      <p>Click link below to activate your account:</p>
+      <a href="${activationLink}">${activationLink}</a>
+      <p>This link expires in 1 hour</p>
+      `,
+    });
+    //response
     return res.status(201).json({
       message: "Regsiter Succes",
       data: {
@@ -101,7 +122,6 @@ export const login = async (req: Request, res: Response) => {
         status: user.status,
       },
     });
-
   } catch (error) {
     if (error instanceof ZodError) {
       return res.status(400).json({
@@ -116,6 +136,50 @@ export const login = async (req: Request, res: Response) => {
     return res.status(500).json({
       message: "Internal Server Error",
       error: error,
+    });
+  }
+};
+
+export const activateToken = async (req: Request, res: Response) => {
+  try {
+    const token = req.params.token;
+
+    if (!token) {
+      return res.status(400).json({
+        message: "Activation token is required",
+      });
+    }
+
+    const user = await prisma.user.findFirst({
+      where: {
+        activationToken: token,
+        activationExpiredAt: {
+          gt: new Date(),
+        },
+      },
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Activation token invailid or expired",
+      });
+    }
+
+    await prisma.user.update({
+      where: { id: user.id },
+      data: {
+        status: "ACTIVE",
+        activationToken: null,
+        activationExpiredAt: null,
+      },
+    });
+
+    return res.status(200).json({
+      meesage: "Account activated successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
     });
   }
 };
--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { register, login } from "../controllers/authController";
+import { register, login, activateToken } from "../controllers/authController";
 
 const authRouter = Router();
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
+authRouter.get("/activate/:token", activateToken);
 
 export default authRouter;
