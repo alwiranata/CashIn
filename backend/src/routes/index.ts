93b904f1b5cb535f55473506271c9fcdfb884228
aldo wiranata <awiranata263@gmail.com>
2026-01-05 21:26:25 +0700

    add jwt middleware

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import authRouter from "./authRoute";
 import transactionRouter from "./transactionRoute";
 import userRouter from "./userRoutes";
+import { jwtMiddleware } from "../middlewares/jwtMiddleware";
 
 const router = Router();
 
 router.use("/auth", authRouter);
-router.use("/user", userRouter);
-router.use("/transaction", transactionRouter);
+router.use("/user", jwtMiddleware, userRouter);
+router.use("/transaction", jwtMiddleware, transactionRouter);
 
 export default router;
