c9a079f95c780083270b769ade2224b1313c30a7
aldo wiranata <awiranata263@gmail.com>
2026-01-09 00:05:19 +0700

    add updateTaskValidation

--- backend/src/validations/taskValidation.ts
+++ backend/src/validations/taskValidation.ts
@@ -7,3 +7,11 @@ export const createTaskValidation = z.object({
   finishTask: z.coerce.date().optional(),
   statusTask: z.enum(["PENDING", "PROGRESS", "DONE"]),
 });
+
+export const updateTaskValidation = z.object({
+  nameTransaction: z.string().min(1, "Name transaction is required").optional(),
+  image: z.string().optional(),
+  startTask: z.coerce.date().optional(),
+  finishTask: z.coerce.date().optional(),
+  statusTask: z.enum(["PENDING", "PROGRESS", "DONE"]).optional(),
+});
