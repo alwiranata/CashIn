58fdca3a14775c71d9d7059ac519177f82553f43
aldo wiranata <awiranata263@gmail.com>
2026-02-04 13:45:21 +0700

    update table trnasaction and user

--- frontend/src/pages/Tables/Transactions.tsx
+++ frontend/src/pages/Tables/Transactions.tsx
@@ -5,16 +5,16 @@ import PageMeta from "../../components/common/PageMeta";
 import TransactionsTable from "../../components/tables/BasicTables/TransactionsTable";
 import AddTransaction from "../../components/addTables/AddTransaction";
 
-export default function Tasks() {
+export default function Transaction() {
   const [reloadKey, setReloadKey] = useState(0);
 
   return (
     <>
-      <PageMeta title="Task" />
-      <PageBreadcrumb pageTitle="Tasks" />
+      <PageMeta title="Transaction" />
+      <PageBreadcrumb pageTitle="Transactions" />
 
       <div className="space-y-6">
-        <ComponentCard title="Task">
+        <ComponentCard title="Transaction">
           <div className="flex justify-end mb-4">
             <AddTransaction onSuccess={() => setReloadKey((k) => k + 1)} />
           </div>
--- frontend/src/pages/Tables/Users.tsx
+++ frontend/src/pages/Tables/Users.tsx
@@ -5,16 +5,16 @@ import PageMeta from "../../components/common/PageMeta";
 import TransactionsTable from "../../components/tables/BasicTables/TransactionsTable";
 import AddUser from "../../components/addTables/AddUsers";
 
-export default function Tasks() {
+export default function User() {
   const [reloadKey, setReloadKey] = useState(0);
 
   return (
     <>
-      <PageMeta title="Task" />
-      <PageBreadcrumb pageTitle="Tasks" />
+      <PageMeta title="User" />
+      <PageBreadcrumb pageTitle="Users" />
 
       <div className="space-y-6">
-        <ComponentCard title="Task">
+        <ComponentCard title="Users">
           <div className="flex justify-end mb-4">
             <AddUser onSuccess={() => setReloadKey((k) => k + 1)} />
           </div>
